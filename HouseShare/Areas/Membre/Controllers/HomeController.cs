24fc79a5cb3f9f9e8c6dc7719d17867667e46ba9
alessiaskia <a.scaccia@interface3.be>
2021-03-18 17:55:13 +0100

    Added Member Area

---
+++ HouseShare/Areas/Membre/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using HouseShare.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HouseShare.Areas.Membre.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Member/Home
+        public ActionResult Index()
+        {
+            if (!SessionUtils.IsLogged) return RedirectToAction("Login", "Account", new { area = "" });
+            return View(SessionUtils.ConnectedUser);
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+    }
+}
